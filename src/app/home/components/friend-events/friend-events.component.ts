433a5603609ed4f2537dbc6d37356256babd44ca
cplan85 <carlos_planchart@live.com>
2022-09-13 17:14:28 +0200

    initial development of trending events slider

--- src/app/home/components/friend-events/friend-events.component.ts
+++ src/app/home/components/friend-events/friend-events.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import KeenSlider, { KeenSliderInstance } from "keen-slider";
 
 @Component({
   selector: 'app-friend-events',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friend-events.component.scss']
 })
 export class FriendEventsComponent implements OnInit {
+  @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
+
+
+
+  slider!: KeenSliderInstance;
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      slides: {
+        perView: 3,
+        spacing: 15,
+      },
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
 
   constructor() { }
 
